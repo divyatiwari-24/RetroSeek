169c7ce4bc872c46e8f1b410d9e58f62e95a01c9
Divya Tiwari <divya.2407.tiwari@gmail.com>
2025-06-12 20:41:25 +0530

    Initial commit for RetroSeek

---
+++ go/core/coreassets/json/results.json.go
@@ -0,0 +1,9 @@
+[
+{{range $i, $e:=.DBResults}}{{if $i}},
+{{end}}  {
+    "URL": "{{.Url}}",
+    "Title": "{{.Title}}",
+    "Snippet": "{{.Body}}",
+    "Description": "{{.Description}}"
+  }{{end}}
+]
---
+++ html/ban/ban.html.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>    
+
+<html>    
+
+  <head>    
+
+    <title>Result</title>    
+
+    <meta http-equiv="content-type"    
+
+        content="text/html; charset=utf-8"/>    
+
+  </head>    
+
+  <body>    
+
+    <p>    
+      Completed.<br><br>
+      <?php echo $output; ?>
+
+    </p>    
+	<a href="ban.php">Return</a>
+  </body>    
+
+</html>
